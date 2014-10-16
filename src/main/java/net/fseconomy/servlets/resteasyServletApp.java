6ce4b96174c721abffa317225dfa29b0e86769a1
Scott Mobley <smobley98@msn.com>
2014-10-15 22:23:45 -0500

    new resteasy json servlet work

--- src/main/java/net/fseconomy/servlets/resteasyServletApp.java
+++ src/main/java/net/fseconomy/servlets/resteasyServletApp.java
@@ -1,8 +1,29 @@
 package net.fseconomy.servlets;
 
-/**
- * Created by smobley on 10/15/2014.
- */
-public class resteasyServletApp
+import java.util.Set;
+import java.util.HashSet;
+import javax.ws.rs.core.Application;
+
+public class resteasyServletApp extends Application
 {
+    private Set<Object> singletons = new HashSet<Object>();
+    private Set<Class<?>> empty = new HashSet<Class<?>>();
+
+    public resteasyServletApp()
+    {
+        singletons.add(new resteasyServlet());
+    }
+
+    @Override
+    public Set<Class<?>> getClasses()
+    {
+        return empty;
+    }
+
+    @Override
+    public Set<Object> getSingletons()
+    {
+        return singletons;
+    }
 }
+
--- src/main/java/net/fseconomy/servlets/resteasyservlet.java
+++ src/main/java/net/fseconomy/servlets/resteasyservlet.java
@@ -1,23 +1,49 @@
 package net.fseconomy.servlets;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import net.fseconomy.data.Data;
+
+import javax.inject.Inject;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.CacheControl;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 /**
- * Created by smobley on 10/15/2014.
+ * A simple REST service which is able to say hello to someone using HelloService Please take a look at the web.xml where JAX-RS
+ * is enabled
+ *
+ * @author gbrey@redhat.com
+ *
  */
-public class resteasyservlet extends HttpServlet
-{
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-    {
 
+@Path("/")
+public class resteasyServlet
+{
+
+    public void init()
+    {
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    @GET
+    @Path("/pilotstatus")
+    @Produces({ "application/json;charset=UTF-8" })
+    public Response getHelloWorldJSON()
     {
+        //This is called A LOT.
+        //The PilotStat class uses single character labels to reduce traffic.
+        List<Data.PilotStatus> list = Data.getInstance().getPilotStatus();
+        CacheControl NoCache = new CacheControl();
+        NoCache.setNoCache(true);
 
+        return Response
+                .status(200)
+                .cacheControl(NoCache)
+                .header("Access-Control-Allow-Origin","*")
+                .entity(list).build();
     }
 }
