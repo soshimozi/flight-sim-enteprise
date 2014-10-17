67f898f3f4f61dfe2d4e2051aa1ec7224926b060
Scott Mobley <smobley98@msn.com>
2014-10-16 23:39:24 -0500

    removed jsonServlet and added methods to resteasyservlet

--- src/main/java/net/fseconomy/servlets/resteasyservlet.java
+++ src/main/java/net/fseconomy/servlets/resteasyservlet.java
@@ -1,12 +1,14 @@
 package net.fseconomy.servlets;
 
 import net.fseconomy.data.Data;
+import net.fseconomy.util.Formatters;
 
 import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.CacheControl;
 import javax.ws.rs.core.Request;
@@ -46,4 +48,46 @@ public class resteasyServlet
                 .header("Access-Control-Allow-Origin","*")
                 .entity(list).build();
     }
+
+    @GET
+    @Path("/fuelquote/{fueltype}/{amount}/{icao}")
+    @Produces({ "application/json;charset=UTF-8" })
+    public Response getGoodsQuote(@PathParam("fueltype") final int fueltype,
+                                  @PathParam("amount") final int amount,
+                                  @PathParam("icao") final String icao)
+    {
+        String price = Formatters.twoDecimals.format(Data.getInstance().quoteFuel(icao, fueltype, amount));
+
+        CacheControl NoCache = new CacheControl();
+        NoCache.setNoCache(true);
+
+        return Response
+                .status(200)
+                .cacheControl(NoCache)
+                .header("Access-Control-Allow-Origin","*")
+                .entity(price).build();
+    }
+
+    @GET
+    @Path("/goodsquote/{goodstype}/{amount}/{icao}/{src}")
+    @Produces({ "application/json;charset=UTF-8" })
+    public Response getGoodsQuote(@PathParam("goodstype") final int goodstype,
+                                  @PathParam("amount") final int amount,
+                                  @PathParam("icao") final String icao,
+                                  @PathParam("src") final int src)
+    {
+        boolean BUY = true;
+
+        String price = Formatters.twoDecimals.format(Data.getInstance().quoteGoods(icao, goodstype, amount, src, BUY));
+
+        CacheControl NoCache = new CacheControl();
+        NoCache.setNoCache(true);
+
+        return Response
+                .status(200)
+                .cacheControl(NoCache)
+                .header("Access-Control-Allow-Origin","*")
+                .entity(price).build();
+    }
+
 }
