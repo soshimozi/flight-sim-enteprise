baa062b63035cf975cd386154b4f3a78ad77ac1d
Scott Mobley <smobley98@msn.com>
2015-01-27 15:47:11 -0600

    Added in longestrwy field.

--- src/main/java/net/fseconomy/dto/AirportInfo.java
+++ src/main/java/net/fseconomy/dto/AirportInfo.java
@@ -6,15 +6,17 @@ public class AirportInfo
     public String name;
     public LatLon latlon;
     public int size;
+    public int longestRwy;
     public int type;
     public int surface;
 
-    public AirportInfo(String icaocode, String airportname, double latitude, double longitude, int sz, int t, int surf)
+    public AirportInfo(String icaocode, String airportname, double latitude, double longitude, int sz, int longestrwy, int t, int surf)
     {
         icao = icaocode;
         name = airportname;
         latlon = new LatLon(latitude, longitude);
         size = sz;
+        longestRwy = longestrwy;
         type = t;
         surface = surf;
     }
