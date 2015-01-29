5cb2bc85ee96e3ac5d3d8467448bac23d48a3774
Scott Mobley <smobley98@msn.com>
2015-01-28 21:11:03 -0600

    AircraftInfo name to title field change

--- src/main/java/net/fseconomy/dto/AirportInfo.java
+++ src/main/java/net/fseconomy/dto/AirportInfo.java
@@ -3,21 +3,21 @@ package net.fseconomy.dto;
 public class AirportInfo
 {
     public String icao;
-    public String name;
+    public String title;
     public LatLon latlon;
     public int size;
     public int longestRwy;
     public int type;
     public int surface;
 
-    public AirportInfo(String icaocode, String airportname, double latitude, double longitude, int sz, int longestrwy, int t, int surf)
+    public AirportInfo(String icaocode, String airportname, double latitude, double longitude, int sz, int longestrwy, int pType, int surf)
     {
         icao = icaocode;
-        name = airportname;
+        title = airportname;
         latlon = new LatLon(latitude, longitude);
         size = sz;
         longestRwy = longestrwy;
-        type = t;
+        type = pType;
         surface = surf;
     }
 }
